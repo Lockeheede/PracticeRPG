1c57d74843c5aaa4dfa58507383b4f7c54c9373b
Lockeheede <prescott.lawani@gmail.com>
2019-11-14 22:18:47 -0600

    19-Implemented the basic movements, as well as the animation with tweaks.

---
+++ PracticeRPG/Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    //It is transform because we need to know the position
+        
+    void LateUpdate()//Using LateUpdate for order of execution
+    //Updating the camera late (after the mover) will make it to where
+    //There is no problem with the camera moving first, before the character
+    //Causing a jittery movement.
+    {
+    transform.position = target.position;
+    }
+}
---
+++ PracticeRPG/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+//Hold Ctrl+Shift+Spacebar when trying to find info on methods
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+        private void MoveToCursor()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            
+            bool hasHit = Physics.Raycast(ray, out hit);
+
+            if (hasHit)
+            {
+                GetComponent<NavMeshAgent>().destination = hit.point;
+            }
+        }
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;//Get velocity from navmesh
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);//Makes the velocity meaningful for the character
+            float speed = localVelocity.z;//Passing it to speed
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);//Put into the animator
+        }
+    
+}
